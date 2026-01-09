b3fa24a229d96e25c8c82cf19519b007fa87981c
gerard meli <melingandjoug@gmail.com>
2026-01-09 09:35:35 +0100

    MicroService

---
+++ Kotlin/quizservice/src/main/kotlin/com/example/quizservice/QuizserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.quizservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QuizserviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<QuizserviceApplication>(*args)
+}
