69d3255b7ee9aab97bd1857d8a09c3e35d8a8389
gerard meli <melingandjoug@gmail.com>
2026-01-12 13:30:27 +0100

    Microservice/apigateway

--- Kotlin/questionapp/src/main/kotlin/com/reli237/questionapp/QuestionappApplication.kt
+++ Kotlin/questionapp/src/main/kotlin/com/reli237/questionapp/QuestionappApplication.kt
@@ -2,8 +2,10 @@ package com.reli237.questionapp
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class QuestionappApplication
 
 fun main(args: Array<String>) {
